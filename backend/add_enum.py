489e81bbf11288ea067df35b9a20d935241bc6bb
Priti Paudel <preetipaudel456@gmail.com>
2026-08-02 16:44:42 +0545

    removed/unnecessaryfiles

--- backend/add_enum.py
+++
@@ -1,8 +0,0 @@
-import sys; sys.path.append('d:/gre_e_preparation/backend')
-import logging; logging.disable(logging.CRITICAL)
-from database import engine
-import sqlalchemy as sa
-with engine.connect() as conn:
-    conn.execute(sa.text("ALTER TYPE definitionsource ADD VALUE IF NOT EXISTS 'book_unverified'"))
-    conn.commit()
-print('Enum updated.')
